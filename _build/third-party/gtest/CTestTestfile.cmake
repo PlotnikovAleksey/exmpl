47d6c2e3a8e6b13bdb1b447e41869bd166ab0915
PlotnikovAleksey <alex280201@gmail.com>
2019-09-03 21:41:53 +0300

    added CI

--- _build/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fbdn/PlotnikovAleksey/workspace/my_example/third-party/gtest
-# Build directory: /home/fbdn/PlotnikovAleksey/workspace/my_example/_build/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
